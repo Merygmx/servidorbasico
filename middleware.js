db705368519e132f846f11b90c22fbec2a5e72b2
Merygmx <merygmx@gmail.com>
2024-12-13 19:59:48 -0600

    actualización de middleware.js

--- middleware.js
+++ middleware.js
@@ -3,3 +3,17 @@ const app=express();
 const port=3000;
 
 //middleware para registrar solicitudes
+const logger=(req,res,next)=>{
+    console.log(`{req.method} ${req.url}`);
+    next(); //pasa a la sig ruta de ejecución
+};
+
+app.use(logger);
+
+app.get('/',(req,res)=>{
+    res.send('Mensaje enviado');
+});
+
+app.listen(port,()=>{
+    console.log(`Este es el servidor corriendo en http://localhost:${port}`);
+});
